08429d3e47dc4cf9893bcea5fbbe15aee25561ec
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-21 15:13:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,6 @@ def  introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language
-  puts "Hi, my name is #{name}.
-  I am learning to program in #{language}."
+def introduction_with_language(name)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
